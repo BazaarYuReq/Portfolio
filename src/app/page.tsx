c9ee17ae6ddc0180b87cf45939264832fb9241f8
BazaarYuReq <bilegdemberelochirbat@gmail.com>
2026-01-13 23:07:02 +0800

    Improved design layout and card fully implemented soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,13 @@ import { FaInstagram, FaFacebook, FaDiscord, FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="w-[100vw] h-[100vh] flex flex-row ">
-      <section className="bg-purple-800 w-[25vw] h-[100vh] flex flex-col justify-between items-center">
+    <main className="w-[100vw] h-[100vh] flex flex-row bg-black">
+      <section className="bg-black w-[25vw] h-[100vh] text-white border-yellow-500 border-3 flex flex-col justify-between items-center">
       
       <div className="bg-black w-[22vw] h-[40vh] border-2 rounded-3xl m-[20px]" ></div>
 
-      <div className="text-2xl">
-
-        <p>My social accounts</p>
-
-      </div>
- <div className="flex gap-5 justify-center mt-6 items-center bg-purple-100 w-[25vw] rounded-3xl py-[20px]">
-<div className="flex gap-4">
+ <div className="flex justify-center mt-6 items-center bg-yellow-500 w-[22vw] rounded-3xl pt-[20px] pb-[30px] translate-y-[-100px]">
+<div className="flex">
  <div className="flex gap-4 ">
   {/* Instagram */}
   <a
@@ -134,212 +129,86 @@ export default function Home() {
       "
     >
       <FaGithub className="text-4xl" />
+      
+    </div>
+  </a>
+  
+  <a
+    href="https://github.com/BazaarYuReq"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="relative inline-block cursor-pointer"
+  >
+    <div className="absolute inset-0 rounded-2xl bg-neutral-300 shadow-[0_8px_0_rgb(180,180,180)]"></div>
+    <div
+      className="
+        relative
+        bg-white
+        rounded-2xl
+        p-4
+        flex items-center justify-center
+        text-gray-900
+        hover:text-gray-800
+        translate-y-0
+        shadow-[0_6px_0_rgb(200,200,200),0_10px_10px_rgba(0,0,0,0.05)]
+        transition-all duration-150 ease-out
+        hover:translate-y-[-2px]
+        hover:shadow-[0_10px_0_rgb(200,200,200),0_14px_12px_rgba(0,0,0,0.08)]
+        active:translate-y-0
+        active:scale-[0.97]
+      "
+    >
+      <FaGithub className="text-4xl" />
+      
     </div>
   </a>
 </div>
+    
 
+     
+   
 </div>
 
+
 </div>
-    <div className="w-[22vw] h-[25vh] bg-black"></div>
+    <div className="w-[22vw] h-[25vh] bg-black"><section className=" bg-black translate-y-[-130px]">
+      <h2 className="text-3xl text-yellow-500 font-bold font-sans text-center "> My Skills</h2>
+    <section className="grid grid-cols-3 gap-8 w-[22vw] h-[33vh] mx-auto place-items-center  bg-black ">
+  <img src="/languages/html.svg" alt="HTML" width="128" height="128" />
+  <img src="/languages/css.svg" alt="CSS" width="128" height="128" />
+  <img src="/languages/javascript.svg" alt="JavaScript" width="128" height="128" />
+  <img src="/languages/typescript.svg" alt="TypeScript" width="128" height="128" />
+  <img src="/languages/react.svg" alt="React" width="128" height="128" />
+  <img src="/languages/nodejs.svg" alt="NodeJS" width="128" height="128" />
+</section></section></div>
 
     </section>
-    <section className="w-[75vw] h-[100vh] bg-amber-500">
-      <h1 className="text-5xl text-purple-700 font-bold font-sans"> My Skills</h1>
-    <section className="grid grid-cols-4 gap-8 w-[75vw] mx-auto place-items-center border-5 rounded-3xl bg-white">
-  <img src="/languages/html.svg" alt="HTML" width="64" height="64" />
-  <img src="/languages/css.svg" alt="CSS" width="64" height="64" />
-  <img src="/languages/javascript.svg" alt="JavaScript" width="64" height="64" />
-  <img src="/languages/typescript.svg" alt="TypeScript" width="64" height="64" />
-  <img src="/languages/react.svg" alt="React" width="64" height="64" />
-  <img src="/languages/nodejs.svg" alt="NodeJS" width="64" height="64" />
-  <img src="/languages/nodejs.svg" alt="NodeJS" width="64" height="64" />
-  <img src="/languages/nodejs.svg" alt="NodeJS" width="64" height="64" />
+<section
+className="w-[75vw] h-[100vh] text-yellow-500 text-center">
+<div >
+  <h1 className="text-5xl">My Coding Journey</h1>
+  <p>August 2025,
+    I started my coding journey and I knew nothing about coding with the basic table row and column knowledge to do my bidding in ICT subject I studied in 9th and 10th grade
+  </p>
+  <p>September 2025,
+    This is where I started picking up normal web developing skills in web building using HTML and CSS. During this time I was making new friends in my coding bootcamp to discuss about our projects we're going to make.
+  </p>
+  <p>October 2025,
+    This month was hard as to learn my JavaScript with my studies in subject almost catching up
+  </p>
+  <p>November 2025,
+    The first project started with my teammates but I struggled in building the app using NextJS and React, only coming up with the routing to other pages and normal popups on click to save it in local memory. I almost gave up here to do coding, as many of my friends knew how to do it better than mine. Espeically after hacktoberfest after my team lost 3rd place in close tie </p>
+  <p>December 2025,
+    I've kept creating many small projects to increase my coding skill but especially for my team's project and making one with the longest time of tampering becoming the project but I started my own project almost at december, in which my personal project must be created for my graduation from the coding bootcamp. So I came up with a great idea to save time and develop it more unique and better than any other projects, I started a project named PersonalPlanner. At first it was a mess with bad ui and messy as my before projects. Then I decided to just work on making it visiually pleasing with its own style so I basically made a laptop inside of an computer.
+  </p>
+  <p>January 2025,
+    I finished my coding bootcamp with my team project and personal project being 29/30, which I didn't think about achieving such feat. I have overestimated my friends skill over mine. I engineered my project more unique and complex yet so simple than my friends. Now, I'm thinking about making my project use barely any back-end to be client-side focused to turn it into MVP as a solo till I find people to work on my project with me. That way I can have my project well known around the world with genius engineering as a beginner coder.
+
+  </p>
+  <p>Now I'm working on my projects to implement my MVP and work with many tech stack to create many projects that will benefit not only me but many millions of people around the world. Internshipping at my coding academy to level my skill further and learn coding further with python, rust and redis. But till June I might not dedicate as much as I did before as I have to lock in.</p>
+</div>
+
 </section>
-
-      <div className="flex flex-row gap-10">
-<div className="relative">
-  {/* Fixed base */}
-  <div
-    className="
-      absolute inset-0
-      rounded-2xl
-      bg-neutral-300
-      shadow-inner
-    "
-  />
-
-  {/* Button face */}
-  <div
-    className="
-      relative
-      bg-white
-      rounded-2xl
-      p-6
-      border border-neutral-200
-
-      translate-y-[-2px]
-      shadow-[0_6px_0_rgb(200,200,200)]
-
-      transition-all
-      duration-100
-      ease-out
-
-      hover:translate-y-[-2px]
-      hover:shadow-[0_12px_0_rgb(200,200,200)]
-
-      active:translate-y-0
-      active:shadow-[0_2px_0_rgb(200,200,200)]
-    "
-  >
-    {/* content */}
-    <h3 className="font-semibold text-lg">What I do</h3>
-    <p className="text-sm text-neutral-600">
-    I work with HTML, CSS, JavaScript, TypeScript, React, and Next.js, focusing
-    mainly on frontend development while learning full-stack skills.
-    </p>
-  </div>
-</div>
-<a
-  href="https://github.com/BazaarYuReq"
-  target="_blank"
-  rel="noopener noreferrer"
-  class="flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2"
->
-  <span
-    class="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40"
-  ></span>
-
-  <div class="flex items-center">
-    <svg class="w-4 h-4 fill-current" viewBox="0 0 438.549 438.549">
-      <path
-        d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z"
-      ></path>
-    </svg>
-    <span class="ml-1 text-white">Star on GitHub</span>
-  </div>
-
-  <div class="ml-2 flex items-center gap-1 text-sm md:flex">
-    <svg
-      class="w-4 h-4 text-gray-500 transition-all duration-300 group-hover:text-yellow-300"
-      aria-hidden="true"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        clip-rule="evenodd"
-        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-        fill-rule="evenodd"
-      ></path>
-    </svg>
-    <span class="inline-block tabular-nums tracking-wider font-display font-medium text-white">6</span>
-  </div>
-</a>
-<div class="w-[190px] h-[254px] perspective">
-  <div class="relative w-full h-full text-center transition-transform duration-700 transform-style-preserve-3d group hover:rotate-y-180">
-
-    <div class="absolute w-full h-full flex flex-col justify-center items-center bg-gradient-to-br from-bisque/80 via-[rgb(255,231,222)]/80 to-[rgb(255,211,195)]/80 text-coral shadow-lg border border-coral rounded-xl backface-hidden">
-      <p class="text-xl font-extrabold m-0">FLIP CARD</p>
-      <p>Hover Me</p>
-    </div>
-  
-    <div class="absolute w-full h-full flex flex-col justify-center items-center bg-gradient-to-br from-[rgb(255,174,145)]/80 via-coral/80 to-bisque/80 text-white shadow-lg border border-coral rounded-xl backface-hidden rotate-y-180">
-      <p class="text-xl font-extrabold m-0">BACK</p>
-      <p>Leave Me</p>
-    </div>
-  </div>
-</div>
-
-
-<div className="relative">
-  {/* Fixed base */}
-  <div
-    className="
-      absolute inset-0
-      rounded-2xl
-      bg-neutral-300
-      shadow-inner
-    "
-  />
-
-  {/* Button face */}
-  <div
-    className="
-      relative
-      bg-white
-      rounded-2xl
-      p-6
-      border border-neutral-200
-
-      translate-y-[-2px]
-      shadow-[0_6px_0_rgb(200,200,200)]
-
-      transition-all
-      duration-100
-      ease-out
-
-      hover:translate-y-[-2px]
-      hover:shadow-[0_12px_0_rgb(200,200,200)]
-
-      active:translate-y-0
-      active:shadow-[0_2px_0_rgb(200,200,200)]
-    "
-  >
-    {/* content */}
-    <h3 className="font-semibold text-lg">What I do</h3>
-    <p className="text-sm text-neutral-600">
-    I work with HTML, CSS, JavaScript, TypeScript, React, and Next.js, focusing
-    mainly on frontend development while learning full-stack skills.
-    </p>
-  </div>
-</div>
-
-<div className="relative">
-  {/* Fixed base */}
-  <div
-    className="
-      absolute inset-0
-      rounded-2xl
-      bg-neutral-300
-      shadow-inner
-    "
-  />
-
-  {/* Button face */}
-  <div
-    className="
-      relative
-      bg-white
-      rounded-2xl
-      p-6
-      border border-neutral-200
-
-      translate-y-[-2px]
-      shadow-[0_6px_0_rgb(200,200,200)]
-
-      transition-all
-      duration-100
-      ease-out
-
-      hover:translate-y-[-2px]
-      hover:shadow-[0_12px_0_rgb(200,200,200)]
-
-      active:translate-y-0
-      active:shadow-[0_2px_0_rgb(200,200,200)]
-    "
-  >
-    {/* content */}
-    <h3 className="font-semibold text-lg">What I do</h3>
-    <p className="text-sm text-neutral-600">
-    I work with HTML, CSS, JavaScript, TypeScript, React, and Next.js, focusing
-    mainly on frontend development while learning full-stack skills.
-    </p>
-  </div>
-</div>
-
-      </div>
-    </section>
 
     </main>
   );
